1b448f3427cb56399a173041f197cffb0fbfb421
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-11-20 22:34:20 +0300

    Task 47

--- Task047/Program.cs
+++ Task047/Program.cs
@@ -5,4 +5,42 @@
 //              1 -3,3 8 -9,9
 //              8 7,8 -7,1 9
 
-Console.WriteLine("Hello, World!");
+double[,] FillMatrixRnd(int rows, int colums, double min, double max)
+{
+    double[,] matrix = new double [rows, colums];
+    Random rnd  = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i,j] = Math.Round((rnd.NextDouble()*(max-min)+min), 2);
+            }
+    } 
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) -1 ) Console.Write($"{matrix[i,j], 8} |");
+            else Console.Write($"{matrix[i,j], 8}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+Console.WriteLine("Введите количество строк:");
+int matrixRows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int matrixColums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное ограничение массива:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите максимальное ограничение массива:");
+double b = Convert.ToDouble(Console.ReadLine())+1;
+double [,] myMatrix = FillMatrixRnd(matrixRows, matrixColums, a, b);
+PrintMatrix(myMatrix);
+Console.WriteLine(" ");
